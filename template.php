3a876ab091498d4a9b63ed43e4ae9a790170e3f9
katelyndb <97479562+katelyndb@users.noreply.github.com>
2022-09-26 14:37:10 -0600

    Setting things up some more

---
+++ template.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href='css/main.css'>
+    <title>Nia's Survey!</title>
+</head>
+<body>
+    <header><php include 'modularization/header.php'; ?></header>
+    <main>
+        <!-- <php include 'CONTENTNAME'; ?></header> -->
+    </main>
+    <footer> <php include 'modularization/footer.php'; ?> </header>
+    
+</body>
+</html>
--- testing.php
+++ testing.php
@@ -30,8 +30,95 @@ $people = array(
 // If / Conditional Statements
 if ($color[0]== 'Purple' and 5 > 1) {
     echo 'Yep it is purple!';
-}elseif {
+} elseif ($color[0]== 'Blue') {
+    echo  'It is BLUE!';
+}else {
     echo 'Nope it not';
-};
+}
+// Loops \
+$i = 1;
+while ( $i < 5) {
+    echo 'YEET';
+    $i++;
+}
+$changing = 1;
+do {
+    echo $changing + ' WOW';
+    $changing++;
+} while ( $changing < 3 );
+for ($i = 0; $i < 4; $i++) {echo 'Another loop wow';}
+foreach ($colors as $item) { echo $item;}
+// INCLUDE AND REQUIRE
+
+/* 
+    Inside the header of the file
+    <php include 'header.php'; ?>
+    
+    Require will produce a fatal error if failure to retrieve
+    <php require 'header.php'; ?>
+*/
+
+// Functions
+function cool_boy($name = 'Default Name') {
+    echo $name + 'Stay cooool boiiii';
+}
+cool_boy('Bob');
+
+function another_function($food, $place) {
+    echo $food + ' from ' + $place;
+    return 1;
+}
+echo another_function('bacon','the store');
+
+// Super Global Variables
+/*
+    $_SERVER
+    $GLOBALS
+    $_REQUEST
+    $_POST
+    $_GET
+    $_FILES
+    $_ENV
+    $_COOKIE
+    $_SESSION
+*/
+
+// Working with Files
+$my_file = fopen('cool_file.txt', 'w');
+fwrite($my_file, 'My added text wow');
+fclose($my_file);
+
+// Reading
+
+$another_file = file('awesome_file.txt');
+foreach ($another_file as $line) {
+    echo $line;
+}
+
+// Classes
+class Dog {
+    public $breed; // property
+    public function fetch() {
+        // method
+        echo 'Plays fetch';
+    }
+}
+$emily = new Dog(); // instantiating
+$emily ->breed = "Black Lab"; // assigning
+echo $emily ->breed;
+$emily ->fetch();
+
+// THIS
+class Cat {
+    public $breed; // property
+    public $age;
+    public function info() {
+        // method
+        echo 'Breed' + $this -> breed;
+        echo 'Age' + $this -> age;
+    }
+
+}
+
 
 ?>
