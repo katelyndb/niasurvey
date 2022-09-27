fdfc43bfbacbdefa69783013d413918b5e20d05d
katelyndb <97479562+katelyndb@users.noreply.github.com>
2022-09-26 22:02:59 -0600

    Starting the SQL File to create the table!

---
+++ sql/niasurvey.sql
@@ -0,0 +1,23 @@
+-- Nia Survey SQL Creation 
+
+CREATE TABLE people (
+    LastName VARCHAR(20),
+    Firstname VARCHAR(20),
+    age INT,
+    gender ENUM ('F','M'),
+)
+
+
+CREATE TABLE survey (
+    FavColor ENUM ('Red','Orange', 'Yellow', 'Green', 'Blue', 'Indigo' 'Purple', 'Magenta', 'Cyan', 'Thursday', 'White', 'Black', 'Gray', 'Brown', 'Pink'),
+    Food VARCHAR(20),
+    ProudOf VARCHAR(20),
+    Class VARCHAR(20),
+    Song VARCHAR(20),
+    PizzaTopping ENUM ('Sausage','Pineapple', 'Peppers', 'Onions', 'Tomatoes', 'Pepperoni' 'Ham', 'Cheese', 'Olives', 'Mushrooms',),
+    Holiday ENUM ("New Year's Day", 'Groundhogs Day', "Valentine's Day", "St. Patrick's Day", "April Fool's Day", "Easter", "Earth Day", "Juneteenth", "Memorial Day", "Fourth of July", "Halloween", "Thanksgiving", "Christmas"),
+    Drink
+    TimeOfDay
+    LeastColor
+)
+
